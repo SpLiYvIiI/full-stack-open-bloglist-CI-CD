60d4028658838b4dd22693ee005209932615c161
SpLiYvIiI <atukadagio123@gmail.com>
2021-05-25 15:42:37 +0400

    delete global settings

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   'env': {
     'browser': true,
-    'es6': true,
-    'jest/globals': true ,
-    'cypress/globals': true
+    'es6': true
   },
   'extends': [
     'eslint:recommended',
